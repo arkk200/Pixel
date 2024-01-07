64e97e1719e4bbc57e6e29a98050bdec4a07b989
이명재 <arkk20000@gmail.com>
2024-01-08 00:17:05 +0900

    feat: Board 퍼블리싱

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,9 +5,30 @@ import Tiles from "./Tiles.vue";
 <template>
   <div class="container">
     <div class="top-slider"></div>
-    <Tiles />
+    <Tiles class="tiles" />
     <div class="side-slider"></div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  background-color: white;
+  box-shadow: 3px 3px 100px 1px rgba(77, 71, 71, 0.5);
+  width: min-content;
+  border-radius: 6rem;
+  display: grid;
+  grid-template-rows: 6rem min-content 6rem;
+  grid-template-columns: 6rem min-content 6rem;
+}
+.tiles {
+  grid-row: 2 / 3;
+  grid-column: 2 / 3;
+}
+.top-slider {
+  grid-column: 2 / 3;
+}
+.side-slider {
+  grid-row: 2 / 3;
+  grid-column: 3 / 4;
+}
+</style>
