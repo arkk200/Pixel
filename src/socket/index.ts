83bb7b3d0f34aed333dce7a2c66711780d264bac
이명재 <arkk20000@gmail.com>
2024-01-16 23:10:45 +0900

    feat: 누군가 연결이 끊겼을 때 메인 화면으로 돌아오는 코드 구현

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,3 +1,4 @@
+import { GAME_OVER_DATA_MESSAGE } from "@src/constants";
 import router from "@src/router";
 import { store } from "@src/stores";
 import { io } from "socket.io-client";
@@ -8,9 +9,20 @@ export const socket = io(serverURL);
 socket.on("startGame", (gameData) => {
   store.state.isInGame = true;
   store.state.gameData = gameData;
-  router.push("play");
+  router.push("/play");
 });
 
 socket.on("updateGame", (gameData) => {
   store.state.gameData = gameData;
+});
+
+socket.on("gameOver", async ({ message, winPlayer, order }) => {
+  if (message === GAME_OVER_DATA_MESSAGE.SOMEONE_DISCONNECTED) {
+    alert("누군가가 게임을 나갔습니다.");
+    router.push("/");
+    store.state.isInGame = false;
+  }
+  if (message === GAME_OVER_DATA_MESSAGE.SOMEONE_WON) {
+    alert(`${winPlayer.playerName} (${order})가 승리했습니다.`);
+  }
 });
