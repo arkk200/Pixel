cdfeb6c6029c1692a57eb6ffde7208585362c6f0
이명재 <arkk20000@gmail.com>
2024-01-18 02:24:15 +0900

    feat: room 페이지 컴포넌트 언마운트 시 방 떠나는 소켓 연결

--- src/router/room.vue
+++ src/router/room.vue
@@ -3,6 +3,17 @@ import Logo from "@src/components/common/Logo.vue";
 import AppLayout from "@src/components/layouts/AppLayout.vue";
 import RoomAction from "@src/components/pages/room/RoomAction.vue";
 import WaitingPlayerList from "@src/components/pages/room/WaitingPlayerList.vue";
+import { socket } from "@src/socket";
+import { State } from "@src/types";
+import { onUnmounted } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore<State>();
+
+onUnmounted(() => {
+  store.state.isInRoom = false;
+  socket.emit("leaveRoom");
+});
 </script>
 
 <template>
