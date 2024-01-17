a54d8f3ffa9697ee9978e3d9a5d903d8d1810a33
이명재 <arkk20000@gmail.com>
2024-01-18 02:41:02 +0900

    feat: 게임 떠나기 소켓 연결

--- src/router/play.vue
+++ src/router/play.vue
@@ -2,6 +2,16 @@
 import AppLayout from "@src/components/layouts/AppLayout.vue";
 import Board from "@src/components/pages/play/Board/Board.vue";
 import UserInterface from "@src/components/pages/play/UserInterface/UserInterface.vue";
+import { socket } from "@src/socket";
+import { State } from "@src/types";
+import { onUnmounted } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore<State>();
+onUnmounted(() => {
+  store.state.isInGame = false;
+  socket.emit("leaveGame");
+});
 </script>
 
 <template>
