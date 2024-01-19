3d922b21835528bd49510563883993f0e752a9d3
이명재 <arkk20000@gmail.com>
2024-01-20 00:12:40 +0900

    refactor: CSS 스타일 분리

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import "vue3-toastify/dist/index.css";
 import App from "./App.vue";
 import router from "./router";
 import { store } from "./stores";
-import "./style.css";
+import "./styles/font.css";
+import "./styles/normalize.css";
 
 createApp(App).use(store).use(router).use(Vue3Toastify).mount("#app");
