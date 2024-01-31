43334a035a199b7b013b82539510eac7fb566461
이명재 <arkk20000@gmail.com>
2024-01-31 23:24:31 +0900

    feat: 로고 이미지 파일 추가

--- src/components/common/Logo.vue
+++ src/components/common/Logo.vue
@@ -6,8 +6,7 @@ const { size = 16 } = defineProps<{ size?: CSSProperties["font-size"] }>();
 
 <template>
   <div class="logo" :style="{ fontSize: size }">
-    <img src="/assets/pixel-board.png" class="logo-image" />
-    <p class="logo-title"><span>PIXEL.IO</span></p>
+    <img src="/assets/pixel-logo.png" class="logo-image" />
   </div>
 </template>
 
@@ -19,26 +18,6 @@ const { size = 16 } = defineProps<{ size?: CSSProperties["font-size"] }>();
   position: relative;
 }
 .logo-image {
-  width: 18em;
-}
-.logo-title {
-  position: absolute;
-  font-size: 4em;
-  font-weight: 900;
-}
-.logo-title span {
-  position: relative;
-  background-image: url(/assets/wood.jpg);
-  -webkit-background-clip: text;
-  background-clip: text;
-  color: transparent;
-}
-.logo-title::before {
-  position: absolute;
-  content: "PIXEL.IO";
-  top: 0;
-  left: 0;
-  mix-blend-mode: hard-light;
-  text-shadow: 0 0 0.6em rgba(0, 0, 0, 0.65);
+  width: 21.5em;
 }
 </style>
